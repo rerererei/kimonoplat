572a1a1adcffb9efd4377e3b701383bb05ab0e73
ishibashi <ishibababa@gmail.com>
2023-04-10 22:41:14 +0900

    テーマ追加

---
+++ wp-content/themes/kimonoplat/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?> 
+<div class="contents">
+  test site
+</div><!--end contents--> 
+<?php get_footer(); ?>
---
+++ wp-content/themes/kimonoplat/page-about.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: about
+*/
+?>
+<?php get_header(); ?>
+<div class="contents">
+</div>
+<?php get_footer(); ?>

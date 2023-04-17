eabd9f8ecab5f9280e586dc6be97716890411988
ishibashi <ishibashi@fubright.co.jp>
2023-04-17 23:42:50 +0900

    画像フォルダ作成、css作成、css読み込み追記

--- wp-content/themes/kimonoplat/header.php
+++ wp-content/themes/kimonoplat/header.php
@@ -9,8 +9,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/resources/js/oss-function.js" type="text/javaScript" charset="utf-8"></script>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/oss-page.css">
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/resources/js/kmnp-function.js" type="text/javaScript" charset="utf-8"></script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/kmnp-common.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/kmnp-top.css">
 </head>
 
 <body <?php body_class(); ?>>

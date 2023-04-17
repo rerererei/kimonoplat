7a906de17b9ec7a7a981c97d4a6c1724e2671e6b
ishibashi <ishibashi@fubright.co.jp>
2023-04-17 23:36:38 +0900

    ignore test

--- wp-content/themes/kimonoplat/footer.php
+++
@@ -1,8 +0,0 @@
-<!--end footer-inner-->
-</footer>
-</div> <!--end wrapper--> 
-<?php wp_footer(); ?>
-<!--システム・プラグイン用-->
-</body>
- 
-</html>
--- wp-content/themes/kimonoplat/index.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?> 
-<div class="contents">
-  kmnp-top
-</div><!--end contents--> 
-<?php get_footer(); ?>

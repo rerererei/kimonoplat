d3023eaa291dc6d61b9c13306826e92b344a1da4
imai.daichi <daichi.imai@fubright.co.jp>
2023-04-22 13:33:44 +0900

    おすすめ記事カードスタイル調整

--- wp-content/themes/kimonoplat/index.php
+++ wp-content/themes/kimonoplat/index.php
@@ -1,243 +1,244 @@
-<?php get_header(); ?> 
+<?php get_header(); ?>
 
 <div class="contents">
-  <!-- おすすめ記事　ここから -->
-<div class="top-osusume">
-    <div class="top-midashi"></div>
-    <div class="arts-osusume">
-      <div class="art-card-sq">
-        <img src="" alt="おすすめ記事">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
+    <!-- おすすめ記事　ここから -->
+    <div class="top-osusume">
+        <div class="top-midashi"></div>
+        <div class="arts-osusume">
+            <div class="art-card-sq">
+                <img src="" alt="おすすめ記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="おすすめ記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="おすすめ記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="おすすめ記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="おすすめ記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="おすすめ記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="おすすめ記事">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="おすすめ記事">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="おすすめ記事">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="おすすめ記事">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="おすすめ記事">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <a href="" class="link-osusume-itiran"><p class="link-osusume-itiran-arrow">→</p>おすすめ記事一覧を見る</a>
+        <a href="" class="link-osusume-itiran">
+            <p class="link-osusume-itiran-arrow">→</p>おすすめ記事一覧を見る
+        </a>
     </div>
-  </div>
-  <!-- おすすめ記事　ここまで -->
+    <!-- おすすめ記事　ここまで -->
 
 
-  <!-- よく読まれている記事　ここから -->
-  <div class="top-ranking">
-    <div class="top-midashi"></div>
-    <div class="arts-ranking">
-    <div class="art-card-bar">
-        <img src="" alt="1位">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-          <img src="" alt="特集">
+    <!-- よく読まれている記事　ここから -->
+    <div class="top-ranking">
+        <div class="top-midashi"></div>
+        <div class="arts-ranking">
+            <div class="art-card-bar">
+                <img src="" alt="1位">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                    <img src="" alt="特集">
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="art-card-bar">
-        <img src="" alt="2位">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-          <img src="" alt="特集">
+        <div class="art-card-bar">
+            <img src="" alt="2位">
+            <div class="art-summary">
+                <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                <div class="card-tags">
+                    <a href="" class="tag-text">#着物</a>
+                    <a href="" class="tag-text">#なんやら</a>
+                </div>
+                <div class="card-timestamp"><img src=>2023/3/10</div>
+                <img src="" alt="特集">
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="art-card-bar">
-        <img src="" alt="3位">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-          <img src="" alt="特集">
+        <div class="art-card-bar">
+            <img src="" alt="3位">
+            <div class="art-summary">
+                <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                <div class="card-tags">
+                    <a href="" class="tag-text">#着物</a>
+                    <a href="" class="tag-text">#なんやら</a>
+                </div>
+                <div class="card-timestamp"><img src=>2023/3/10</div>
+                <img src="" alt="特集">
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <!-- よく読まれている記事　ここまで -->
+    <!-- よく読まれている記事　ここまで -->
 
 
-  <!-- 特集　ここから -->
-  <div class="top-feature">
-    <div class="top-midashi"></div>
-    <div class="arts-feature">
-      <div class="art-card-sq">
-        <img src="" alt="特集">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="特集">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-      <div class="art-card-sq">
-        <img src="" alt="特集">
-        <div class="art-summary">
-          <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-          <div class="card-tag">
-            <a href="" class="tag-text">着物</a>
-            <a href="" class="tag-text">なんやら</a>
-          </div>
-          <div class="card-timestamp"><img src=>2023/3/10</div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- 特集　ここまで -->
-
-
-  <!-- 新着記事　ここから -->
-  <div class="top-new">
-    <div class="top-midashi"></div>
-    <div class="arts-new">
-      <div class="art-card-sq">
-          <img src="" alt="新着記事">
-          <div class="art-summary">
-            <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-            <div class="card-tag">
-              <a href="" class="tag-text">着物</a>
-              <a href="" class="tag-text">なんやら</a>
+    <!-- 特集　ここから -->
+    <div class="top-feature">
+        <div class="top-midashi"></div>
+        <div class="arts-feature">
+            <div class="art-card-sq">
+                <img src="" alt="特集">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
             </div>
-            <div class="card-timestamp"><img src=>2023/3/10</div>
-          </div>
-        </div>
-        <div class="art-card-sq">
-          <img src="" alt="新着記事">
-          <div class="art-summary">
-            <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-            <div class="card-tag">
-              <a href="" class="tag-text">着物</a>
-              <a href="" class="tag-text">なんやら</a>
+            <div class="art-card-sq">
+                <img src="" alt="特集">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
             </div>
-            <div class="card-timestamp"><img src=>2023/3/10</div>
-          </div>
-        </div>
-        <div class="art-card-sq">
-          <img src="" alt="新着記事">
-          <div class="art-summary">
-            <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-            <div class="card-tag">
-              <a href="" class="tag-text">着物</a>
-              <a href="" class="tag-text">なんやら</a>
+            <div class="art-card-sq">
+                <img src="" alt="特集">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
             </div>
-            <div class="card-timestamp"><img src=>2023/3/10</div>
-          </div>
-        </div>
-        <div class="art-card-sq">
-          <img src="" alt="新着記事">
-          <div class="art-summary">
-            <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-            <div class="card-tag">
-              <a href="" class="tag-text">着物</a>
-              <a href="" class="tag-text">なんやら</a>
-            </div>
-            <div class="card-timestamp"><img src=>2023/3/10</div>
-          </div>
-        </div>
-        <div class="art-card-sq">
-          <img src="" alt="新着記事">
-          <div class="art-summary">
-            <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-            <div class="card-tag">
-              <a href="" class="tag-text">着物</a>
-              <a href="" class="tag-text">なんやら</a>
-            </div>
-            <div class="card-timestamp"><img src=>2023/3/10</div>
-          </div>
-        </div>
-        <div class="art-card-sq">
-          <img src="" alt="新着記事">
-          <div class="art-summary">
-            <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
-            <div class="card-tag">
-              <a href="" class="tag-text">着物</a>
-              <a href="" class="tag-text">なんやら</a>
-            </div>
-            <div class="card-timestamp"><img src=>2023/3/10</div>
-          </div>
         </div>
     </div>
-  </div>
-  <!-- 新着記事　ここまで -->
-</div><!--end contents--> 
+    <!-- 特集　ここまで -->
+
+
+    <!-- 新着記事　ここから -->
+    <div class="top-new">
+        <div class="top-midashi"></div>
+        <div class="arts-new">
+            <div class="art-card-sq">
+                <img src="" alt="新着記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="新着記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="新着記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="新着記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="新着記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+            <div class="art-card-sq">
+                <img src="" alt="新着記事">
+                <div class="art-summary">
+                    <h1 class="card-midashi">着物の着付け初心者でも簡単に着るコツやらなんやらを紹介</h1>
+                    <div class="card-tags">
+                        <a href="" class="tag-text">#着物</a>
+                        <a href="" class="tag-text">#なんやら</a>
+                    </div>
+                    <div class="card-timestamp"><img src=>2023/3/10</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 新着記事　ここまで -->
+</div>
+<!--end contents-->
 <?php get_footer(); ?>
